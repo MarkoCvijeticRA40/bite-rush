85286c5c12a0ca362f5056042139393701e129f8
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2025-04-23 22:07:38 +0200

    Added product price errors and change product error

--- src/Domain/Products/ProductErrors.cs
+++ src/Domain/Products/ProductErrors.cs
@@ -3,7 +3,7 @@
 namespace Domain.Products;
 public static class ProductErrors
 {
-    public static Error NotFound(Guid productId) => Error.NotFound(
+    public static Error NotFound(string productId) => Error.NotFound(
         "Products.NotFound",
         $"The product with the Id = '{productId}' was not found");
 
---
+++ src/Domain/Products/ProductPriceErrors.cs
@@ -0,0 +1,13 @@
+﻿using SharedKernel;
+
+namespace Domain.Products;
+public static class ProductPriceErrors
+{
+    public static Error NotFound(string productPriceId) => Error.NotFound(
+        "ProductPrices.NotFound",
+        $"The product price with the Id = '{productPriceId}' was not found");
+
+    public static Error Unauthorized() => Error.Failure(
+        "ProductPrices.Unauthorized",
+        "You are not authorized to perform this action.");
+}
