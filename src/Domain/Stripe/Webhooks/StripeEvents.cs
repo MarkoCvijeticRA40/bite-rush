1d342b1a68c19c57776cd0810fe99dfa36d531cf
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2025-04-23 22:08:36 +0200

    Allowed react app and created stripe events

---
+++ src/Domain/Stripe/Webhooks/StripeEvents.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Stripe.Webhooks;
+public static class StripeEvents
+{
+    // Product events
+    public const string ProductCreated = "product.created";
+    public const string ProductUpdated = "product.updated";
+    public const string ProductDeleted = "product.deleted";
+
+    //Price events
+    public const string ProductPriceCreated = "product.price.created";
+    public const string ProductPriceUpdated = "product.price.updated";
+    public const string ProductPriceDeleted = "product.price.deleted";
+}
