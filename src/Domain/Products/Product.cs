4babcd3bf9569de6c704b7dbfdd2a1b2488e163a
MarkoCvijeticRA40 <93485029+MarkoCvijeticRA40@users.noreply.github.com>
2025-04-27 17:27:11 +0200

    Change product and product price model

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -1,14 +1,15 @@
 ﻿using SharedKernel;
 
-namespace Domain.Product;
+namespace Domain.Products;
 public sealed class Product : Entity
 {
-    public Guid Id { get; set; }
+    public string Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public long Price { get; set; }
-    public string PriceId { get; set; }
     public long Updated { get; set; }
     public bool Active { get; set; }
+    public bool LiveMode { get; set; }
     public List<string> Images { get; set; } = new();
+    //Navigation Properties
+    public ICollection<ProductPrice> Prices { get; set; }
 }
